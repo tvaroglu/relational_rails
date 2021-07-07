7752c94f0925789220256f63b9a5c69f950f34fc
Taylor Varoglu <tava8398@colorado.edu>
2021-07-06 19:50:10 -0600

    Add Region to db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Region.destroy_all
+
+us_rocky_mountain = Region.create!(
+  name: "US - Rocky Mountain",
+  active: true,
+  rvp_operations: 'Fred "Shreddy" McGnar',
+  priority: 1)
+us_northeast = Region.create!(
+  name: "US - Northeast",
+  active: true,
+  rvp_operations: 'Hillary Adapspad',
+  priority: 2)
+us_west_coast = Region.create!(
+  name: "US - West Coast",
+  active: true,
+  rvp_operations: 'Stacey Cohen',
+  priority: 1)
+canada = Region.create!(
+  name: "Canada",
+  active: true,
+  rvp_operations: 'Stephen Abootman',
+  priority: 1)
+apac = Region.create!(
+  name: "APAC",
+  active: true,
+  rvp_operations: 'Steven Chu',
+  priority: 2)
+eu = Region.create!(
+  name: "EU",
+  active: true,
+  rvp_operations: 'Klaus Heisler',
+  priority: 1)
+latam = Region.create!(
+  name: "LATAM",
+  active: false,
+  rvp_operations: 'Gustavo Fring',
+  priority: 3)

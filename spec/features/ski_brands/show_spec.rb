f1e6d0b33b26378e5052924fac56ce09cc40593c
Alex Klick <aklick12@gmail.com>
2021-07-06 18:03:28 -0600

    us test 2 complete

--- spec/features/ski_brands/show_spec.rb
+++ spec/features/ski_brands/show_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'ski brands Show' do
-
+  it 'has only the parent with given id' do
+    atomic = SkiBrand.create!(name:'Atomic', does_racing:'true', year_founded:1955)
+    head = SkiBrand.create!(name:'Head', does_racing:'true', year_founded:1950)
+    line = SkiBrand.create!(name:'Line', does_racing:'false', year_founded:1995)
+    armada = SkiBrand.create!(name:'Armada', does_racing:'false', year_founded:2002)
+    visit "/ski_brands/#{atomic.id}"
+    expect(page).to have_content(atomic.name)
+    expect(page).to_not have_content(head.name)
+    expect(page).to_not have_content(armada.name)
+    expect(page).to_not have_content(line.name)
+  end
 end

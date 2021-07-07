d9fa3ce48320df9b8af1c319dbb1eda572074663
Antonio King <antonio.j.king@gmail.com>
2021-07-06 22:21:15 -0600

    Add the parks index

---
+++ db/migrate/20210707002622_create_park.rb
@@ -0,0 +1,14 @@
+class CreatePark < ActiveRecord::Migration[5.2]
+  def change
+    create_table :parks do |t|
+      t.string :name
+      t.string :state
+      t.string :county
+      t.integer :parking_fee
+      t.integer :parking_capacity
+      t.boolean :dogs_allowed
+
+      t.timestamps
+    end
+  end
+end

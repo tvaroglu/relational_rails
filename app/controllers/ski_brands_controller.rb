97113ae88b91c6dae213811acd521801183cb7b2
Alex Klick <aklick12@gmail.com>
2021-07-07 11:23:09 -0600

    passed US 1 ski_brands index

--- app/controllers/ski_brands_controller.rb
+++ app/controllers/ski_brands_controller.rb
@@ -1,2 +1,5 @@
 class SkiBrandsController < ApplicationController
+  def index
+    @brands = SkiBrand.all
+  end
 end
--- spec/features/ski_brands/index_spec.rb
+++ spec/features/ski_brands/index_spec.rb
@@ -3,24 +3,31 @@ require 'rails_helper'
 RSpec.describe 'ski brands Index' do
 
   before(:each) do 
-    atomic = SkiBrand.create!(name:'Atomic', does_racing:'true', year_founded:1955)
-    head = SkiBrand.create!(name:'Head', does_racing:'true', year_founded:1950)
-    line = SkiBrand.create!(name:'Line', does_racing:'false', year_founded:1995)
-    armada = SkiBrand.create!(name:'Armada', does_racing:'false', year_founded:2002)
+    @atomic = SkiBrand.create!(name:'Atomic', does_racing:'true', year_founded:1955)
+    @head = SkiBrand.create!(name:'Head', does_racing:'true', year_founded:1950)
+    @line = SkiBrand.create!(name:'Line', does_racing:'false', year_founded:1995)
+    @armada = SkiBrand.create!(name:'Armada', does_racing:'false', year_founded:2002)
+    @bentChetler = Ski.create!(name:'Bent Chetler 100', for_jumps:'true', turn_radius:19.5, ski_brand_id: @atomic.id)
+    @vantage = Ski.create!(name:'Vantage 90 Ti', for_jumps:'false', turn_radius:18.4, ski_brand_id: @atomic.id)
+    @arv106 = Ski.create!(name:'ARV 106', for_jumps:'true', turn_radius:20.5, ski_brand_id: @armada.id)
   end
 
   it 'has all of the ski brands' do
+
     visit '/ski_brands'
-    expect(page).to have_content(atomic.name)
-    expect(page).to have_content(head.name)
-    expect(page).to have_content(armada.name)
-    expect(page).to have_content(line.name)
+
+    expect(page).to have_content(@atomic.name)
+    expect(page).to have_content(@head.name)
+    expect(page).to have_content(@armada.name)
+    expect(page).to have_content(@line.name)
   end
 
   it 'is sorted by most recently created' do
+
     visit '/ski_brands'
-    expect(atomic.name).to appear_before(head.name)
-    expect(head.name).to appear_before(line.name)
-    expect(line.name).to appear_before(armada.name)
+
+    expect(@atomic.name).to appear_before(@head.name)
+    expect(@head.name).to appear_before(@line.name)
+    expect(@line.name).to appear_before(@armada.name)
   end
 end

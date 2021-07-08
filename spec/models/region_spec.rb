fac2549fad0e2209f4bb630ed49e3c811986f601
Taylor Varoglu <tava8398@colorado.edu>
2021-07-07 20:35:44 -0600

    Add relationship tests for Region and Resort models

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Region do
   describe 'relationships' do
-    it {should have_many(:resorts)}
+    # it {should have_many(:resorts)}
+    it 'has many children' do
+      region = Region.reflect_on_association(:resorts)
+      expect(region.macro).to eq(:has_many)
+    end
   end
 
 end
--- spec/models/resort_spec.rb
+++ spec/models/resort_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Resort do
   describe 'relationships' do
-    it {should belong_to(:region)}
+    # it {should belong_to(:region)}
+    it 'belongs to a region' do
+      resort = Resort.reflect_on_association(:region)
+      expect(resort.macro).to eq(:belongs_to)
+    end
   end
 
 end

7f8d7383b6e5ecbd02cc9a514612e77bf3dd97dd
Antonio King <antonio.j.king@gmail.com>
2021-07-08 23:13:15 -0600

    Add park show

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/parks', to: 'parks#index'
+  get 'parks/:id', to: 'parks#show'
   get '/trails', to: 'trails#index'
 end
---
+++ spec/features/parks/show_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'User Story 2 - Park Show' do
+  # As a visitor
+  # When I visit '/parents/:id'
+  # Then I see the parent with that id including the parent's attributes:
+  # - data from each column that is on the parent table
+  it 'shows the specified park and lists attributes' do
+    park = Park.create!(name: "North Table Mountain",
+                        state: "CO",
+                        county: "Jefferson",
+                        parking_fee: 0,
+                        parking_capacity: 100,
+                        dogs_allowed: true)
+    visit "/parks/#{park.id}"
+
+    expect(page).to have_content(park.name)
+    expect(page).to have_content("State: #{park.state}")
+    expect(page).to have_content("County: #{park.county}")
+    expect(page).to have_content("Parking Fee: $#{park.parking_fee}")
+    expect(page).to have_content("Parking Capacity: #{park.parking_capacity}")
+    expect(page).to have_content("Dogs Allowed: #{park.dogs_allowed}")
+  end
+end

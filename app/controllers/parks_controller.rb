14bd0d6537997e3f4a1a438a1800bad4f7155dcd
Antonio King <antonio.j.king@gmail.com>
2021-07-09 21:35:44 -0600

    Add parks show

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -2,4 +2,8 @@ class ParksController < ApplicationController
   def index
     @parks = Park.all
   end
+
+  def show
+    @park = Park.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/parks', to: 'parks#index'
+  get '/parks/:id', to: 'parks#show'
 
   get '/regions', to: 'regions#index'
   get '/regions/:id', to: 'regions#show'

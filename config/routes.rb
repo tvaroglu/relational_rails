7145f2e14533a89bcf90c5850d82552249011c88
Antonio King <antonio.j.king@gmail.com>
2021-07-10 11:44:50 -0600

    Add parks trails index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/parks', to: 'parks#index'
   get '/parks/:id', to: 'parks#show'
+  get '/parks/:park_id/trails', to: 'park_trails#index'
 
   get '/trails', to: 'trails#index'
   get '/trails/:id', to: 'trails#show'
